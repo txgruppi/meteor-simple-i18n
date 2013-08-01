08437016bb7aa3180637127c13db032dbb85216a
txgruppi <txgruppi@gmail.com>
2013-08-01 09:59:53 -0300

    Updated to 0.2.0

--- i18n-view-helper.js
+++ i18n-view-helper.js
@@ -1,20 +1,20 @@
 // A simple Meteor internationalization
 //
 // @author Tarcísio Gruppi <txgruppi@gmail.com>
-// @version 0.1.7
+// @version 0.2.0
 // @date 2013-03-27
 
 (function(){
   // Register a Handlebars helper named `t`
   // Try to translate a string using `I18n.prototype.t`
-  // 
+  //
   // Use `_.str.sprintf` to return a formated string
-  // 
+  //
   // Usage:
   // <pre>&lt;template name="greeting"&gt;
   //   {{t "Hi, %s", userName}}
   // &lt;/template&gt;</pre>
-  // 
+  //
   // @see `I18n.prototype.t`
   Handlebars.registerHelper('t', function(object){
     var args = Array.prototype.slice.call(arguments, 0, arguments.length - 1);
