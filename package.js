6335cdaa9de4e3eeab7ca59101ccf210528a0b5b
txgruppi <txgruppi@gmail.com>
2013-08-26 19:41:30 -0300

    Add api.use handlebars

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 // A simple Meteor internationalization
 //
 // @author Tarcísio Gruppi <txgruppi@gmail.com>
-// @version 0.3.0
+// @version 0.3.1
 // @date 2013-03-27
 
 // Package description
@@ -11,6 +11,7 @@ Package.describe({
 
 // On use
 Package.on_use(function (api) {
+  api.use(["handlebars"]);
   // Add files
   api.add_files("i18n.js", ["client", "server"]);
   api.add_files("i18n-view-helper.js", "client");
